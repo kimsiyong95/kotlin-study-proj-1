b782342a41e08522b3297aaabac40552e34919f5
kimsiyong95 <kimsiyong95@gmail.com>
2022-07-28 21:22:05 +0900

    feat : 코틀린 학습

---
+++ src/main/kotlin/Study-17.kt
@@ -0,0 +1,52 @@
+/**
+ * 코틀린에서 람다를 다루는 방법
+ */
+
+
+class Fruit(
+    val name: String,
+    val price: Int
+)
+
+fun main(){
+    val fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("바나나", 2000),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 4000),
+        Fruit("바나나", 5000),
+        Fruit("수박", 6000),
+        Fruit("수박", 7000),
+        Fruit("수박", 8000),
+        Fruit("사과", 9000),
+    )
+
+    val isApple: (Fruit) -> Boolean = fun(fruit: Fruit): Boolean{ // 이름 없는 함수 람다
+        return fruit.name == "사과"
+    }
+    
+    // (파라미터타입) -> 반환타입
+    val isApple2: (Fruit) -> Boolean = {fruit: Fruit -> fruit.name == "사과"} // 자주쓰는 표현법
+    val isApple3 = {fruit: Fruit -> fruit.name == "사과"} // 자주쓰는 표현법
+                                                        // 람다는 마지막줄이 반환값임
+
+    println(isApple(fruits[0]))
+    println(isApple.invoke(fruits[0]))
+    println(isApple2(fruits[0]))
+    println(isApple2.invoke(fruits[0]))
+
+    filterFruits(fruits, isApple2)
+    filterFruits(fruits) { it.name == "사과"} // 소괄호에서 중괄호를 빼면 마지막 파라미터로 인식
+                                            // 파라미터가 1개일 경우 it으로 대체 가능 
+}
+
+private fun filterFruits(fruits: List<Fruit>, filter: (Fruit) -> Boolean): List<Fruit> {
+    val results = mutableListOf<Fruit>()
+
+    for(fruit in fruits){
+        if(filter(fruit)){
+            results.add(fruit)
+        }
+    }
+    return results
+}
